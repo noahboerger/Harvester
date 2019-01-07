26846d527c57dc6df74b1dc00edb44f450488952
noahboerger <35099943+noahboerger@users.noreply.github.com>
2019-01-07 14:52:49 +0100

    Field

--- src/Cutting.java
+++ src/Cutting.java
@@ -1,4 +1,15 @@
 public class Cutting {
+    private boolean isOn;
 
+    public Cutting() {
+        isOn = false;
+    }
 
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void setOn(boolean on) {
+        isOn = on;
+    }
 }
--- src/Field.java
+++ src/Field.java
@@ -1,2 +1,16 @@
 public class Field {
+    Wheat[][] wheatField;
+
+    public Field() {
+        wheatField  = new Wheat[10000][10000];
+        for(int x = 0; x < 10000; x++) {
+            for(int y = 0; y < 10000; y++) {
+                wheatField[x][y] = new Wheat();
+            }
+        }
+    }
+
+    public Wheat scanWheatAtPosition(int x, int y) {
+        return wheatField[x][y];
+    }
 }
